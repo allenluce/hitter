8f0aede79f4beeb9a9c95d7f310eff30faa9e4f1
Allen Luce <aluce@github.con.com>
2016-12-12 14:50:08 -0800

    Add production Mongo DB data.

--- common/prod.go
+++ common/prod.go
@@ -5,11 +5,15 @@
 package common
 
 var (
-	MONGOSTRING = "mongodb://" +
-		"svc_rc:EHDgcFeuk7cKCZ0b@rhythmcontent-shard-00-00-ptf9c.mongodb.net," +
-		"svc_rc:EHDgcFeuk7cKCZ0b@rhythmcontent-shard-00-01-ptf9c.mongodb.net," +
-		"svc_rc:EHDgcFeuk7cKCZ0b@rhythmcontent-shard-00-02-ptf9c.mongodb.net" +
-		"?replicaSet=RhythmContent-shard-0"
+	DBS = map[string]string{
+		"local": "mongodb://localhost",
+		"old":   "mongodb://mongo.lyfemobile.net:27017/rtbtest",
+		"newdb": "mongodb://" +
+			"svc_rc:EHDgcFeuk7cKCZ0b@rhythmcontent-shard-00-00-ptf9c.mongodb.net," +
+			"svc_rc:EHDgcFeuk7cKCZ0b@rhythmcontent-shard-00-01-ptf9c.mongodb.net," +
+			"svc_rc:EHDgcFeuk7cKCZ0b@rhythmcontent-shard-00-02-ptf9c.mongodb.net" +
+			"?replicaSet=RhythmContent-shard-0",
+	}
 
 	MONGODB = "rtbtest"
 	WEBPORT = 80
